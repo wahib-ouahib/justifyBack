f985027400ad345596fbd0c1030877cbeb4978cf
wahib <wahib.med.ouahib@gmail.com>
2018-11-27 23:59:42 +0000

    handling more errors

--- app.js
+++ app.js
@@ -6,11 +6,21 @@ const mongoose = require('mongoose');
 //seting up express
 const app = express();
 
+//handling the Mongoose promise connection
+mongoose.Promise = global.Promise;
+
 //Connecting to mongoose
-mongoose.connect('mongodb://wahib:' +process.env.MONGO_ATLAS_PW+ '@justifybackdb-shard-00-00-k2n4k.mongodb.net:27017,justifybackdb-shard-00-01-k2n4k.mongodb.net:27017,justifybackdb-shard-00-02-k2n4k.mongodb.net:27017/test?ssl=true&replicaSet=JustifyBackDB-shard-0&authSource=admin&retryWrites=true',
-{
-    useNewUrlParser: true
-});
+const dbUrl = 'mongodb://wahib:'+process.env.MONGO_ATLAS_PW+'@cluster0-shard-00-00-auv4t.mongodb.net:27017,cluster0-shard-00-01-auv4t.mongodb.net:27017,cluster0-shard-00-02-auv4t.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+mongoose.connect(dbUrl,{useNewUrlParser: true});
+
+//handling connection to Mongodb Atlas errors
+mongoose.connection.on('connected', () => {    
+    console.log(`mongoose connection open to dbUrl`); 
+ });
+ 
+ mongoose.connection.on('error', (err) => {    
+   console.log(`mongoose connection err: `, err); 
+ });
 
 //body parser (using body whith a ContentType of text/plain)
 app.use(bodyParser.urlencoded({extended: true})); 
@@ -20,20 +30,10 @@ app.use(bodyParser.json());
 //using routes on routes file form of URL/api/... (Check api.js for middleware)
 app.use('/api',routes);
 
-//handling errors
-app.use((req, res, next)=>{
-    const error = new Error('Not found');
-    error.status = 404;
-    next(error);
+//error handling middleware
+app.use(function(err, req, res, next){
+    res.status(422).send({error: err.message});
 })
-app.use((error, req, res, next)=>{
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message
-        }
-    })
-});
 
 //specifying the port
 const myPort = 4000 || process.env.port;
