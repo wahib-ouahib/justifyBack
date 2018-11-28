8b6f9f8013e837b48e52e5e2652dfd86a2bc046f
wahib <wahib.med.ouahib@gmail.com>
2018-11-28 21:08:02 +0000

    :santa: setting up the users database

--- config/users.js
+++ config/users.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 
-
+//defining the users schema
 const userSchema = mongoose.Schema({
     //_id: mongoose.Schema.types.ObjectIdi"
-    email: {type: String, required: true}
+    email: {
+        type: String,
+        required: true,
+        //using the match code to ensure that the mail is in a correct format
+        match: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+        }
 });
 
-//exporting the module
+//exporting the users module
 module.exports = mongoose.model('User', userSchema);
