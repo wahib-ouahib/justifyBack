2d5b451ff641d769ea30489a73a5867e17d78f85
wahib <wahib.med.ouahib@gmail.com>
2018-11-28 23:37:47 +0000

    connecting to mlab and setting up the DB to a heroku deployement

--- app/verifyToken/checkToken.js
+++ app/verifyToken/checkToken.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try{
         const token = req.headers.authorization.split(" ")[1];
-        const decoded = jwt.verify(token, process.env.JWT_KEY);
+        const decoded = jwt.verify(token, "wahib123");
         req.userData = decoded;
         next();
     } catch(error){
