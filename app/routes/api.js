e6406019e9eda874b921adc0fc91828a602804dd
wahib <wahib.med.ouahib@gmail.com>
2018-11-28 15:14:19 +0000

    :ballot_box_with_check: adding api/token route for authentication

--- app/routes/api.js
+++ app/routes/api.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const justify = require('../JustifyAlgo/justify');
 const mongoose = require('mongoose');
 const Texts = require('../../config/db');
-
+const User = require('../../config/users')
 
 //Default GET
 router.get('/justify', (req,res)=>{
@@ -11,28 +11,22 @@ router.get('/justify', (req,res)=>{
     res.end("Please trigger a POST request to justify your text");
 })
 
+//Post route to get a TOKEN
+router.post('/token', (req, res,next) => {
+    const user = new User({email: req.body.email});
+    user.save()
+        .then(result => {
+            res.status(201).json({
+                message: 'User created'
+            });
+        })
+        .catch(next)
+})
+
 //Post the input (unjustified text)
 router.post('/justify', (req, res, next)=>{
     console.log('POST method triggered'); 
     const nbrMots = string_to_array(req.body).length;
-  /*  const text = new Texts({
-        //_id: new mongoose.Types.ObjectId,
-        size: string_to_array(req.body).length
-    })
-    
-    //saving data
-    text.save()
-    .then(result=>{
-        console.log('saved ' + result);
-        
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
-    })*/
-    
     Texts.create({size: string_to_array(req.body).length})
         .then((x)=>console.log('created doc ' + x))
         .catch(next);
