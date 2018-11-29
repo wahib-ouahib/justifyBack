16e840ac7f130670f8559f032000e6e288f57d84
wahib <wahib.med.ouahib@gmail.com>
2018-11-29 02:28:48 +0000

    :zap: give the user a unique token

--- app/verifyToken/checkToken.js
+++ app/verifyToken/checkToken.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try{
         const token = req.headers.authorization.split(" ")[1];
-        const decoded = jwt.verify(token, "wahib123");
+        const decoded = jwt.verify(token, process.env.JWT_KEY);
         req.userData = decoded;
         next();
     } catch(error){
